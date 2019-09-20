a6300d60dadaa06a7b3bc6f0ce8e267909f32a64
EthanJF <ethanjosephfournier@gmail.com>
2019-09-20 17:37:09 -0400

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,34 @@
 class Car
+  attr_accessor :owner, :mechanic
+  attr_reader :make, :model, :classification
 
-  attr_reader :make, :model
+  @@all = []
 
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    classifications = self.all.map do |car|
+      car.classification
+    end
+    classifications.uniq
+  end
+
+  def mechanic_classification
+    Mechanic.all.select do |mechanic|
+      self.classification == mechanic.specialty
+    end
+  end
+    
 
 end
